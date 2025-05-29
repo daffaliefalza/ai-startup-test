8d7595176da0ffeb09a91db9aaa70d001e7bc07c
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-05-30 06:43:39 +0700

    update: button component

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,6 @@
 import LogoIcon from "@/assets/logo.svg";
 import MenuIcon from "@/assets/icon-menu.svg";
+import { Button } from "@/components/Button";
 
 export const Header = () => {
   return (
@@ -30,13 +31,7 @@ export const Header = () => {
           </div>
 
           <div className="flex gap-4 items-center">
-            <button className=" relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
-              <div className="absolute inset-0 ">
-                <div className=" absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
-                <div className=" absolute-inset-0 border-white/40 [mask-image:linear-gradient(to_top, black,transparent)"></div>
-              </div>
-              <span>Join waitlist</span>
-            </button>
+            <Button>Join waitlist</Button>
             <MenuIcon className="md:hidden" />
           </div>
         </div>
