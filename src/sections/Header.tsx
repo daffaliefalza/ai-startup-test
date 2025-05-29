9cedbbd7ae8a48f4f088a5b616ab027003cdeb44
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-05-30 06:17:29 +0700

    initial commit & header section done

---
+++ src/sections/Header.tsx
@@ -0,0 +1,46 @@
+import LogoIcon from "@/assets/logo.svg";
+import MenuIcon from "@/assets/icon-menu.svg";
+
+export const Header = () => {
+  return (
+    <header className="py-4 border-b border-white/15 md:border-none">
+      <div className="container">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
+          <div>
+            <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
+              <LogoIcon className="h-8 w-8" />
+            </div>
+          </div>
+
+          <div className="hidden md:block">
+            <nav className="flex gap-8 text-white/70 ">
+              <a href="#" className="hover:text-white transition">
+                Features
+              </a>
+              <a href="#" className="hover:text-white transition">
+                Developers
+              </a>
+              <a href="#" className="hover:text-white transition">
+                Pricing
+              </a>
+              <a href="#" className="hover:text-white transition">
+                Changelog
+              </a>
+            </nav>
+          </div>
+
+          <div className="flex gap-4 items-center">
+            <button className=" relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
+              <div className="absolute inset-0 ">
+                <div className=" absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
+                <div className=" absolute-inset-0 border-white/40 [mask-image:linear-gradient(to_top, black,transparent)"></div>
+              </div>
+              <span>Join waitlist</span>
+            </button>
+            <MenuIcon className="md:hidden" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
